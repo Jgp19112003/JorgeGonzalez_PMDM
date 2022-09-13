d482656abc9549a7ad9c26ddb21b7593c4aa6e20
Jorge_González-dev <jgp19112003@gmail.com>
2022-09-13 14:25:52 +0200

    .

---
+++ Inicio/src/main/kotlin/FicheroInicial.kt
@@ -0,0 +1,26 @@
+// VARIABLES - FUNCIONES - CLASE
+
+// FUNCIONES fun nombre(arg: Tipo){
+// }
+
+// FUNCIONES fun nombre(arg: Tipo): Tipo retorno{
+// return
+// }
+
+fun main(arg: Array<String>){
+
+    //tipo nombre: Tipo = valor
+    //var (normal) val(constante)
+    //lateinit
+
+    var nombre: String? = null
+    var edad: Int = 18
+    var aficiones: Array<String>;
+    val DNI = "123456789A"
+    nombre = readLine();
+
+    println("Primera ejecucion de Kotlin")
+    println("Hola soy ${nombre} y tengo ${edad} años")
+
+
+}
