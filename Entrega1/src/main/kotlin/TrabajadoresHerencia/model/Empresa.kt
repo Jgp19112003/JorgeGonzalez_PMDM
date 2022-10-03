f8f197a2bb2a1322bc6382e55f7d17315d6fa703
Jorge_González-dev <jgp19112003@gmail.com>
2022-10-03 09:01:26 +0200

    .

---
+++ Entrega1/src/main/kotlin/TrabajadoresHerencia/model/Empresa.kt
@@ -0,0 +1,102 @@
+package TrabajadoresHerencia.model.model
+
+import TrabajadoresHerencia.model.Asalariado
+import TrabajadoresHerencia.model.Autonomo
+import TrabajadoresHerencia.model.Jefe
+import TrabajadoresHerencia.model.Persona
+
+class Empresa() {
+
+    var trabajadores: ArrayList<Persona>?;
+    var jefes: ArrayList<Jefe>?;
+    var trabajador_despedido: Persona? = null
+
+
+    init {
+        trabajadores = ArrayList();
+        jefes = ArrayList(1);
+    }
+
+    fun registrarTrabajador(persona: Persona) {
+
+        trabajadores?.add(persona)
+
+    }
+
+    fun listarTrabajadores(opc: String) {
+
+        trabajadores?.forEach { persona ->
+            when (opc) {
+                "jefes" -> {
+                    if (persona is Jefe) {
+                        persona.mostrarDatos()
+                    }
+                }
+                "asalariados" -> {
+                    if (persona is Asalariado) {
+                        persona.mostrarDatos()
+                    }
+
+                }
+                "autonomos" -> {
+                    if (persona is Autonomo) {
+                        persona.mostrarDatos()
+                    }
+
+                }
+                else -> {
+                    persona.mostrarDatos()
+                }
+            }
+
+        }
+    }
+
+    fun mostrarDatosTrabajador(dni: String) {
+        trabajadores?.forEach { persona ->
+            if (dni.equals(persona.getDNI())) {
+                persona.mostrarDatos()
+            }
+        }
+    }
+
+    fun comprobarDni(dni: String): Int {
+        trabajadores?.forEach { persona ->
+            if (dni.equals(persona.getDNI())) {
+                println("Este DNI ya esta registrado.")
+                return 1;
+            }
+        }
+        return 0;
+    }
+
+    fun despedirTrabajador(dni: String) {
+        trabajadores?.forEach{ persona ->
+            if (dni.equals(persona.getDNI()) && persona is Jefe) {
+                var dni_empleado: String? = null
+                println("Introduzca DNI del trabajador a despedir: ")
+                dni_empleado = readLine()?.toString()
+<<<<<<< HEAD:Entrega1/src/main/kotlin/model/Empresa.kt
+                trabajadores?.forEach{ trabajador ->
+                    if (dni_empleado == trabajador.getDNI()){
+                       trabajadores!!.remove(trabajador)
+=======
+                trabajadores?.forEach { trabajador ->
+                    if (dni_empleado == trabajador.getDNI()) {
+                        trabajador_despedido = trabajador
+>>>>>>> 1fd87b286737edb129d1e59d0e648ff2002f552a:Entrega1/src/main/kotlin/TrabajadoresHerencia/model/Empresa.kt
+                        println("Trabajador despedido")
+                    } else {
+                        println("El trabajador no existe.")
+
+                    }
+                }
+            } else {
+                println("Solo puede realizar un despido un trabajador.")
+            }
+        }
+        trabajadores!!.remove(trabajador_despedido)
+    }
+}
+
+
