86346841dcd9a7d61b5762c4a2eec733c72fb3bc
Jgp19112003 <92918174+Jgp19112003@users.noreply.github.com>
2023-02-22 19:18:06 +0100

    .

---
+++ T5/PracticaDialogos2/app/src/main/java/model/Usuario.kt
@@ -0,0 +1,8 @@
+package com.example.practicadialogos2.model
+
+class Usuario (var nombre: String, var apellido: String, var correo: String, var edad: String, var puesto: String) {
+
+    override fun toString(): String {
+        return super.toString()
+    }
+}
